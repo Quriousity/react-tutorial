1d3fff85b25a342ffb12e5fdbfbcfe2435249fe2
bizzy91 <developer.bizzy@gmail.com>
2022-12-12 16:49:27 +0900

    props
    비구조화 할당

--- react-tutorial/src/components/Hello.jsx
+++ react-tutorial/src/components/Hello.jsx
@@ -1,5 +1,5 @@
-function Hello(props) {
-  return <div style={{ color: props.color }}>안녕하세요 {props.name}</div>
+function Hello({ color, name }) {
+  return <div style={{ color }}>안녕하세요 {name}</div>
 }
 
 export default Hello;

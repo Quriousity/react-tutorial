1b41e00f1d1a3c66af2d8c193300edd947b2b220
bizzy91 <developer.bizzy@gmail.com>
2022-12-12 17:16:07 +0900

    Counter
    onClick 에 함수 연결하기

--- react-tutorial/src/components/Counter.jsx
+++ react-tutorial/src/components/Counter.jsx
@@ -1,9 +1,16 @@
 function Counter() {
+  const onIncrease = () => {
+    console.log("+1")
+  }
+  const onDecrease = () => {
+    console.log("-1")
+  }
+
   return (
     <div>
       <h1>0</h1>
-      <button>+1</button>
-      <button>-1</button>
+      <button onClick={onIncrease}>+1</button>
+      <button onClick={onDecrease}>-1</button>
     </div>
   )
 }

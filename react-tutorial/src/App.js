b7249744842d44859451a42030607be9f5e20333
bizzy91 <developer.bizzy@gmail.com>
2022-12-12 17:13:07 +0900

    Counter
    Counter.jsx

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,16 +1,11 @@
-import Hello from './components/Hello'; 
-import Hello2 from './components/Hello2';
 import Wrapper from './components/Wrapper';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div>
       <Wrapper>
-        <Hello name="react" color="tomato" isSpecial/>
-        <Hello name="Javascript"/>
-        <Hello />
-        <Hello2 name="react" color="grey"/>
-        <Hello2 name="react" color="grey" isSpecial/>
+        <Counter />
       </Wrapper>
     </div>
   );
---
+++ react-tutorial/src/components/Counter.jsx
@@ -0,0 +1,11 @@
+function Counter() {
+  return (
+    <div>
+      <h1>0</h1>
+      <button>+1</button>
+      <button>-1</button>
+    </div>
+  )
+}
+
+export default Counter;

94b179bb05bf5b358fd7d6aa3f7367d69ae4287e
bizzy91 <developer.bizzy@gmail.com>
2022-12-12 16:56:37 +0900

    props
    Wrapper

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,11 +1,14 @@
 import Hello from './components/Hello'; 
+import Wrapper from './components/Wrapper';
 
 function App() {
   return (
     <div>
-      <Hello name="react" color="tomato"/>
-      <Hello name="Javascript"/>
-      <Hello />
+      <Wrapper>
+        <Hello name="react" color="tomato"/>
+        <Hello name="Javascript"/>
+        <Hello />
+      </Wrapper>
     </div>
   );
 }
